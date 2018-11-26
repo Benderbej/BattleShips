eb42fb4a3112530a125bb3126a0e868fc37e6129
Ostap Strashevskii <strashevskyseo@gmail.com>
2018-11-26 16:11:48 +0300

    text

--- src/ru/javabit/ship/FleetsDisposal.java
+++ src/ru/javabit/ship/FleetsDisposal.java
@@ -12,7 +12,6 @@ public class FleetsDisposal {
     FleetAutoDisposer enemyAutoDisposer;
     FleetAutoDisposer playerManualDisposer;//TODO
     FleetAutoDisposer enemyManualDisposer;//TODO
-
     GameField gameField;
 
     public FleetsDisposal(GameField gameField, Fleet fleet1, Fleet fleet2) {
