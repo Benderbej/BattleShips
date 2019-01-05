51c404869a1c812ff73f0231134c5010191d9f00
Ostap Strashevskii <strashevskyseo@gmail.com>
2019-01-05 18:08:36 +0300

    ClientHandler

--- src/ru/javabit/netgame/server/ClientHandler.java
+++ src/ru/javabit/netgame/server/ClientHandler.java
@@ -1,4 +1,25 @@
 package ru.javabit.netgame.server;
 
+import ru.javabit.GameMath;
+
 public class ClientHandler {
+
+    private int clientServantId;
+
+    public int getClientServantId() {
+        return clientServantId;
+    }
+
+    public void setClientServantId() {
+        clientServantId = GameMath.getRandomInt(1, Integer.MAX_VALUE);
+        //System.out.println("ClientHandler"+clientServantId);
+    }
 }
+
+
+
+
+
+
+
+
