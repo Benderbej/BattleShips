86a5a1bb3e73461c58497114ef61ebe919781f6a
Ostap Strashevskii <strashevskyseo@gmail.com>
2019-01-05 19:49:03 +0300

    ClientGUI

--- src/ru/javabit/netgame/client/ClientGUI.java
+++ src/ru/javabit/netgame/client/ClientGUI.java
@@ -1,4 +1,36 @@
 package ru.javabit.netgame.client;
 
-public class ClientGUI {
+import javax.swing.*;
+import java.awt.*;
+
+public class ClientGUI extends JFrame {
+
+    Client client;
+    JTextArea outTextArea;
+    JTextField inTextField;
+
+    public static void main(String[] args) {
+        ClientGUI clientGUI = new ClientGUI();
+    }
+
+    ClientGUI() {
+        client = new Client();
+        windowConstruct();
+    }
+
+
+    private void windowConstruct(){
+        setSize(400, 500);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setLayout(new BorderLayout());
+        outTextArea = new JTextArea();
+        add(outTextArea);
+        inTextField = new JTextField();
+        add(BorderLayout.SOUTH, inTextField);
+        setVisible(true);
+    }
+
+
+
+
 }
